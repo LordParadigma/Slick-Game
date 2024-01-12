c9d538601e065d0af6fef81d1fff24f11171e4c3
Ishan Shagireev <ishan.shagireev@student.htldornbirn.at>
2024-01-12 09:45:50 +0100

    Ordner erstellt und easyGames hinzugefügt

--- src/at/is/game/EasyGame.java
+++ src/at/is/game/EasyGame.java
@@ -1,5 +1,50 @@
 package at.is.game;
 
-public class EasyGame {
+import org.newdawn.slick.*;
+import org.newdawn.slick.tiled.TiledMap;
+
+
+public class EasyGame extends BasicGame {
+    Font font;
+
+    public EasyGame(String title) {
+        super(title);
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new EasyGame("EasyGame"));
+            container.setDisplayMode(1024,768,false);
+            container.setMinimumLogicUpdateInterval(25);
+            container.setTargetFrameRate(60);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Create a new image rendering test
+     */
+
+    public void init(GameContainer container) throws SlickException {
+
+
+    }
+
+
+
+
+    public void update(GameContainer container, int delta) {
+        Input input = container.getInput();
+        // Fenster mit ESC schließen
+        if (input.isKeyPressed(Input.KEY_ESCAPE)) {
+            container.exit();
+        }
+
+    }
+
+
+    public void render(GameContainer container, Graphics g) {
+    }
 
 }
